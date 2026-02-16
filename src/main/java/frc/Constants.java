d7cac9d1941a505eff86adaf585787aa2b70d082
Jarek Rettinghouse <j.rettinghouse@gmail.com>
2026-02-15 17:15:50 -0800

    Pid Constant Placeholders for 2 systems

--- src/main/java/frc/Constants.java
+++ src/main/java/frc/Constants.java
@@ -1,5 +1,19 @@
 package frc;
 
 public final class Constants{
+    public static final class TurretConstants {
+        public static final double TURRET_P = 0.0;
+        public static final double TURRET_I = 0.0;
+        public static final double TURRET_D = 0.0;
+        public static final double TURRET_KS = 0.0;
+        public static final double TURRET_KV = 0.0;
+    }
 
+    public static final class ShooterConstants {
+        public static final double SHOOTER_P = 0.0;
+        public static final double SHOOTER_I = 0.0;
+        public static final double SHOOTER_D = 0.0;
+        public static final double SHOOTER_KS = 0.0;
+        public static final double SHOOTER_KV = 0.0;
+    }
 }
