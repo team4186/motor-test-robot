1352705de5290216f3fa13ef5f6936c3cadcca2f
Janda95 <jrettinghouseinca@gmail.com>
2026-02-19 20:39:26 -0800

    Updated flexible implementation of motor configs. Intended to quickly swap out systems/motor setups by only creating motors that are needed during robot startup.

--- src/main/java/frc/Constants.java
+++ src/main/java/frc/Constants.java
@@ -1,19 +1,50 @@
 package frc;
 
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.config.SparkBaseConfig;
+
 public final class Constants{
+    public static final class RobotConstants {
+        public static final double NOMINAL_VOLTAGE = 12.0;
+    }
+
     public static final class TurretConstants {
+        // PID
         public static final double TURRET_P = 0.0;
         public static final double TURRET_I = 0.0;
         public static final double TURRET_D = 0.0;
+
+        // FeedForward
         public static final double TURRET_KS = 0.24;
         public static final double TURRET_KV = 0.0;
+        public static final ControlType CLOSED_LOOP_CONTROL_MODE = ControlType.kPosition;
+        public static final int MOTOR_CURRENT_LIMIT = 50; // NEO 550
+        public static final double POSITION_CONVERSION_FACTOR = 18.0; //  (1/Gear_Ratio) * 360 --- (1.0 / 20.0) * 360.0
+        public static final double VELOCITY_CONVERSION_FACTOR = 1.0;
+        public static final double MOTOR_FREE_SPEED = 11000;
+        public static final double MIN_OUTPUT = -0.5;
+        public static final double MAX_OUTPUT = 0.5;
+        public static final SparkBaseConfig.IdleMode IDLE_MODE = SparkBaseConfig.IdleMode.kBrake;
     }
 
     public static final class ShooterConstants {
+        // PID
         public static final double SHOOTER_P = 0.0;
         public static final double SHOOTER_I = 0.0;
         public static final double SHOOTER_D = 0.0;
+
+        // FeedForward
         public static final double SHOOTER_KS = 0.10;
         public static final double SHOOTER_KV = 0.0;
+
+        // CLOSED LOOP CONTROLLER
+        public static final ControlType CONTROL_TYPE = ControlType.kVelocity;
+        public static final int MOTOR_CURRENT_LIMIT = 80; // NEO VORTEX
+        public static final double MOTOR_FREE_SPEED = 6784.0;
+        public static final double POSITION_CONVERSION_FACTOR = 1.0;
+        public static final double VELOCITY_CONVERSION_FACTOR = 1.0;
+        public static final double MIN_OUTPUT = -1.0;
+        public static final double MAX_OUTPUT = 1.0;
+        public static final SparkBaseConfig.IdleMode IDLE_MODE = SparkBaseConfig.IdleMode.kCoast;
     }
 }
---
+++ src/main/java/frc/motors/MotorConfigs.java
@@ -0,0 +1,147 @@
+package frc.motors;
+
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.*;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import frc.Constants.RobotConstants;
+import frc.Constants.ShooterConstants;
+import frc.Constants.TurretConstants;
+
+
+// MotorConfigs Singleton for Subsystem Motors (Swerve Subsystem not included)
+public final class MotorConfigs {
+    private static final MotorConfigs instance = new MotorConfigs();
+
+    // Default Configs
+    private final SparkBaseConfig DefaultSparkMaxConfig = new SparkMaxConfig()
+        .smartCurrentLimit(50)
+        .idleMode(SparkBaseConfig.IdleMode.kBrake);
+
+    private final SparkBaseConfig DefaultSparkFlexConfig = new SparkFlexConfig()
+        .smartCurrentLimit(50)
+        .idleMode(SparkBaseConfig.IdleMode.kBrake);
+
+
+    // private constructor to prevent public class creation
+    private MotorConfigs(){ }
+
+
+    public static MotorConfigs getInstance() { return instance; }
+
+
+    public SparkBaseConfig applyDefaultSparkMaxConfig() { return DefaultSparkMaxConfig; }
+
+
+    public SparkBaseConfig applyDefaultSparkFlexConfig() { return DefaultSparkFlexConfig; }
+
+
+    // build and return custom config
+    public SparkMax applyTurretSparkConfig(
+        SparkMax motor,
+        boolean inverse
+    ) {
+        SparkBaseConfig config = DefaultSparkMaxConfig;
+
+        config
+            .inverted(inverse)
+            .smartCurrentLimit(TurretConstants.MOTOR_CURRENT_LIMIT)
+            .idleMode(TurretConstants.IDLE_MODE);
+
+        config.encoder
+            .positionConversionFactor(TurretConstants.POSITION_CONVERSION_FACTOR)
+            .velocityConversionFactor(TurretConstants.VELOCITY_CONVERSION_FACTOR);
+
+        config.closedLoop
+            .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+            // Set PID values for position control. We don't need to pass a closed loop
+            // slot, as it will default to slot 0.
+            .pid(
+                TurretConstants.TURRET_P,
+                TurretConstants.TURRET_I,
+                TurretConstants.TURRET_D,
+                ClosedLoopSlot.kSlot0)
+            .outputRange(
+                TurretConstants.MIN_OUTPUT,
+                TurretConstants.MAX_OUTPUT,
+                ClosedLoopSlot.kSlot0)
+            .feedForward
+            .kS(
+                TurretConstants.TURRET_KS,
+                ClosedLoopSlot.kSlot0)
+            .kV(
+                RobotConstants.NOMINAL_VOLTAGE / TurretConstants.MOTOR_FREE_SPEED,
+                ClosedLoopSlot.kSlot0); // Nominal * Free Wheel Speed
+
+        motor.configure(
+            config,
+            ResetMode.kResetSafeParameters,
+            PersistMode.kPersistParameters
+        );
+
+        return motor;
+    }
+
+
+    public SparkFlex applyShooterConfig(
+        SparkFlex motorLeader,
+        SparkFlex motorFollower,
+        boolean invertSecondMotor,
+        boolean inverse
+    ){
+        SparkBaseConfig baseConfig = DefaultSparkFlexConfig;
+
+        baseConfig.inverted(inverse)
+            .smartCurrentLimit(ShooterConstants.MOTOR_CURRENT_LIMIT)
+            .idleMode(ShooterConstants.IDLE_MODE);
+
+        // Using Velocity
+        baseConfig.closedLoop
+            .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+            .pid(
+                ShooterConstants.SHOOTER_P,
+                ShooterConstants.SHOOTER_I,
+                ShooterConstants.SHOOTER_D,
+                ClosedLoopSlot.kSlot1)
+            .outputRange(
+                ShooterConstants.MIN_OUTPUT,
+                ShooterConstants.MAX_OUTPUT,
+                ClosedLoopSlot.kSlot1) // Range of total voltage
+            .feedForward
+            // kV is now in Volts, so we multiply by the nominal voltage (12V)
+            .kV(
+                RobotConstants.NOMINAL_VOLTAGE / ShooterConstants.MOTOR_FREE_SPEED,
+                ClosedLoopSlot.kSlot1); // Nominal * desired velocity (12/6784)
+
+        baseConfig.encoder
+            .positionConversionFactor(ShooterConstants.POSITION_CONVERSION_FACTOR)
+            .velocityConversionFactor(ShooterConstants.VELOCITY_CONVERSION_FACTOR);
+
+        motorLeader.configure(
+            baseConfig,
+            ResetMode.kNoResetSafeParameters,
+            PersistMode.kPersistParameters);
+
+
+        SparkBaseConfig followerConfig = new SparkFlexConfig();
+
+        // boolean invertFollower = invertSecondMotor ^ inverse; // using XOR boolean logic
+        boolean invertFollower = inverse;
+        if (invertSecondMotor) {
+            invertFollower = !invertFollower;
+        }
+
+        followerConfig
+            .apply(baseConfig)
+            .follow(motorLeader, invertFollower);
+
+        motorFollower.configure(
+            followerConfig,
+            ResetMode.kNoResetSafeParameters,
+            PersistMode.kPersistParameters);
+
+        return motorLeader;
+    }
+}
